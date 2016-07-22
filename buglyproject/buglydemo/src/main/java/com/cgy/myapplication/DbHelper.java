e200782f55e7a1fc2d1deb1d5118c2c15a660408
cgy <gaoyang.chen@witontek.com>
2016-07-22 16:56:44 +0800

    334

---
+++ buglyproject/buglydemo/src/main/java/com/cgy/myapplication/DbHelper.java
@@ -0,0 +1,33 @@
+package com.cgy.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by RB-cgy on 2016/7/22.
+ */
+public class DbHelper extends SQLiteOpenHelper {
+    public DbHelper(Context context) {
+        super(context, "test", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String a = "CREATE TABLE if not exists [table1] ([nane] TEXT NOT NULL, [pwd] TEXT);";
+        db.execSQL(a);
+        Log.i("a", "db created");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void getList() {
+
+
+    }
+
+}
