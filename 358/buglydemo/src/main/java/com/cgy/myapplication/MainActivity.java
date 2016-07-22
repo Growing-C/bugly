d00eecb94a9162d37f8d1927a249c672cd91a5cd
cgy <gaoyang.chen@witontek.com>
2016-07-22 08:42:24 +0800

    358 399

---
+++ 358/buglydemo/src/main/java/com/cgy/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.cgy.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("aa",null);
+    }
+}
